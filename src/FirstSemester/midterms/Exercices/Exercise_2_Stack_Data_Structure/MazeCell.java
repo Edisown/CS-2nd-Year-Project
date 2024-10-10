5ddac17ddc724d09977b8854cd29bf513fb99a42
Edison Yuki <163733802+Edisown@users.noreply.github.com>
2024-10-10 11:05:07 +0800

    Update MazeCell.java

--- src/FirstSemester/midterms/Exercices/Exercise_2_Stack_Data_Structure/MazeCell.java
+++ src/FirstSemester/midterms/Exercices/Exercise_2_Stack_Data_Structure/MazeCell.java
@@ -3,42 +3,39 @@
  * CS211L - 9344B
  * 10/7/2024
  */
-    package Exercise_2_Stack_Data_Structure;
+package Exercise_2_Stack_Data_Structure;
 
 /**
+ * Template for a cell of a two-dimensional array representing a maze for
+ * simulating the movement of a mouse in a maze
  *
- */
-public class MazeCell {
+ * A cell has a row index and a column index
+ * */
+public class MazeCell{
     private int row;
     private int column;
-
     public MazeCell(){
-        row = 0;
-        column = 0;
+        row=0;
+        column=0;
     }
-
-    public MazeCell(int row, int column) {
+    public MazeCell(int row, int column){
         this.row = row;
         this.column = column;
     }
 
-    public int getRow() {
+    public int getRow(){
         return row;
     }
-
-    public void setRow(int row) {
-        this.row = row;
-    }
-
-    public int getColumn() {
+    public int getColumn(){
         return column;
     }
-
-    public void setColumn(int column) {
+    public void setRow(int row){
+        this.row = row;
+    }
+    public void setColumn(int column){
         this.column = column;
     }
-
-    public boolean sameAs(MazeCell another) {
-        return (row == another.getRow() && column == another.getColumn());
+    public boolean sameAs(MazeCell another){
+        return ( row == another.getRow() && column == another.getColumn());
     }
 }
