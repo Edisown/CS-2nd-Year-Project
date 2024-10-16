0f6569d1ebe75d81808fe7da8348bc4a9b61a192
Edison Malasan <2245504@slu.edu.ph>
2024-10-17 01:13:20 +0800

    added comments

--- src/FirstSemester/midterms/Exercices/Exercise_4_Queue_Simulation/Client.java
+++ src/FirstSemester/midterms/Exercices/Exercise_4_Queue_Simulation/Client.java
@@ -2,57 +2,111 @@ package midterms.queue;
 
 /**
  * Reference class for a typical client of a service system.
- * A client who arrives at a random time (arrivalTime) is assigned number(id).
- * The client will be given service at a certain start time (StartServiceTime)
- *
- * */
-public class Client implements java.lang.Comparable<Client>{
-    private int id;
-    private int arrivalTime;
-    private int startServiceTime;
+ * A client who arrives at a random time (arrivalTime) is assigned a number (id).
+ * The client will be given service at a certain start time (startServiceTime).
+ */
+public class Client implements java.lang.Comparable<Client> {
+    private int id; // Unique identifier for the client
+    private int arrivalTime; // Time when the client arrives
+    private int startServiceTime; // Time when service starts for the client
+
     /**
-     * Construct a client object with a given identification number and arrival time
-     * */
-    public Client(int id, int arrival){
+     * Constructs a client object with a given identification number and arrival time.
+     *
+     * @param id The unique identifier for the client
+     * @param arrival The arrival time of the client
+     */
+    public Client(int id, int arrival) {
         this.id = id;
         this.arrivalTime = arrival;
     }
+
     /**
-     * Construct a client object with a given id, arrival time and start of service time
-     * */
-    public Client(int id, int arrival, int start){
+     * Constructs a client object with a given id, arrival time, and start of service time.
+     *
+     * @param id The unique identifier for the client
+     * @param arrival The arrival time of the client
+     * @param start The start time for the service
+     */
+    public Client(int id, int arrival, int start) {
         this.id = id;
         this.arrivalTime = arrival;
         this.startServiceTime = start;
-
     }
-    public void setId(int n){
+
+    /**
+     * Sets the ID of the client.
+     *
+     * @param n The new ID for the client
+     */
+    public void setId(int n) {
         this.id = n;
     }
-    public void setArrivalTime(int t){
+
+    /**
+     * Sets the arrival time of the client.
+     *
+     * @param t The new arrival time for the client
+     */
+    public void setArrivalTime(int t) {
         this.arrivalTime = t;
     }
-    public void setStartServiceTime(int t){
+
+    /**
+     * Sets the start time for serving the client.
+     *
+     * @param t The start service time for the client
+     */
+    public void setStartServiceTime(int t) {
         this.startServiceTime = t;
     }
-    public int getId(){
+
+    /**
+     * Returns the ID of the client.
+     *
+     * @return The ID of the client
+     */
+    public int getId() {
         return id;
     }
-    public int getArrivalTime(){
+
+    /**
+     * Returns the arrival time of the client.
+     *
+     * @return The arrival time
+     */
+    public int getArrivalTime() {
         return arrivalTime;
     }
-    public int getStartServiceTime(){
+
+    /**
+     * Returns the start time for serving the client.
+     *
+     * @return The start service time
+     */
+    public int getStartServiceTime() {
         return startServiceTime;
     }
-    public int compareTo(Client another){
+
+    /**
+     * Compares this client with another client for ordering.
+     *
+     * @param another The client to compare with
+     * @return A negative integer, zero, or a positive integer
+     *         as this client is less than, equal to, or greater than
+     *         the specified client
+     */
+    public int compareTo(Client another) {
         return (this.toString().compareTo(another.toString()));
     }
+
     /**
-     * Concatenates N, identification number of this client, and a parethesis enclosing
-     * the arrival time of this client then
-     * returns the result of the concatenation
-     * */
-    public String toString(){
-        return ("N"+id +"(" + arrivalTime+")");
+     * Concatenates 'N', the identification number of this client, and a parenthesis enclosing
+     * the arrival time of this client, then returns the result of the concatenation.
+     *
+     * @return A string representation of the client
+     */
+    public String toString() {
+        return ("N" + id + "(" + arrivalTime + ")");
     }
 }
--- src/FirstSemester/midterms/Exercices/Exercise_4_Queue_Simulation/Server.java
+++ src/FirstSemester/midterms/Exercices/Exercise_4_Queue_Simulation/Server.java
@@ -2,14 +2,11 @@ package midterms.queue;
 
 /**
  * Reference class for a typical server (e.g. Teller) in a service system.
- * A server has an assigned number(id).
- * A server serves a client (client)
- * A server has an average service time for the clients (meanServiceTime)
- * A server has a service time for a particular client (serviceTime)
- * A server starts serving a client at a point in time for(startServiceTime)
- * A server finishes servicing a client at a point in time (stopServiceTime)
- * A server has a random number generator that generates service time depending on meanService time
- * */
+ * A server has an assigned number (id) and serves clients with a
+ * specific mean service time. This class models the service
+ * behavior of a server, including the ability to generate
+ * random service times for clients.
+ */
 public class Server implements java.lang.Comparable<Server> {
     private int id;
     private Client client;
@@ -18,10 +15,23 @@ public class Server implements java.lang.Comparable<Server> {
     private int startServiceTime;
     private int stopServiceTime;
     private java.util.Random randomNumberGenerator;
+
+    /**
+     * Default constructor for the Server class.
+     */
     public Server(){
-        id=1;
+        id = 1;
         client = null;
     }
+
+    /**
+     * Parameterized constructor for the Server class.
+     *
+     * @param id Unique identifier for the server
+     * @param client The client being served
+     * @param mean Average service time for clients
+     * @param time Time when service starts
+     */
     public Server(int id, Client client, double mean, int time){
         this.randomNumberGenerator = new java.util.Random();
         this.startServiceTime = time;
@@ -31,50 +41,142 @@ public class Server implements java.lang.Comparable<Server> {
         this.serviceTime = randomNumberGenerator.nextInt((int) Math.ceil(mean));
         this.stopServiceTime = startServiceTime + serviceTime;
     }
+
+    /**
+     * Sets the ID of the server.
+     *
+     * @param n The new ID for the server
+     */
     public void setId(int n){
         this.id = n;
     }
+
+    /**
+     * Sets the client for the server.
+     *
+     * @param client The client to be served by the server
+     */
     public void setClient(Client client){
         this.client = client;
     }
+
+    /**
+     * Sets the mean service time for the server.
+     *
+     * @param mean The new mean service time
+     */
     public void setMeanServiceTime(double mean){
         this.meanServiceTime = mean;
     }
-    public void setServiceTime(int t){
 
+    /**
+     * Sets the service time for the current client.
+     *
+     * @param t The service time for the client
+     */
+    public void setServiceTime(int t){
         this.serviceTime = t;
     }
+
+    /**
+     * Sets the start time for serving the current client.
+     *
+     * @param t The start time for the service
+     */
     public void setStartServiceTime(int t){
         this.startServiceTime = t;
     }
+
+    /**
+     * Sets the stop time for serving the current client.
+     *
+     * @param t The stop time for the service
+     */
     public void setStopServiceTime(int t){
         this.stopServiceTime = t;
     }
+
+    /**
+     * Returns the ID of the server.
+     *
+     * @return The ID of the server
+     */
     public int getId(){
         return id;
     }
+
+    /**
+     * Returns the client being served by the server.
+     *
+     * @return The client being served
+     */
     public Client getClient(){
         return client;
     }
+
+    /**
+     * Returns the mean service time for the server.
+     *
+     * @return The mean service time
+     */
     public double getMeanServiceTime(){
         return meanServiceTime;
     }
+
+    /**
+     * Returns the service time for the current client.
+     *
+     * @return The service time
+     */
     public int getServiceTime(){
         return serviceTime;
     }
+
+    /**
+     * Returns the start time for serving the current client.
+     *
+     * @return The start service time
+     */
     public int getStartServiceTime(){
         return startServiceTime;
     }
+
+    /**
+     * Returns the stop time for serving the current client.
+     *
+     * @return The stop service time
+     */
     public int getStopServiceTime(){
         return stopServiceTime;
     }
+
+    /**
+     * Checks if the server is currently idle (not serving any client).
+     *
+     * @return True if the server is idle, otherwise false
+     */
     public boolean isIdle(){
         return (client == null);
     }
+
+    /**
+     * Compares this server with another server for ordering.
+     *
+     * @param another The server to compare with
+     * @return A negative integer, zero, or a positive integer
+     *         as this server is less than, equal to, or greater than
+     *         the specified server
+     */
     public int compareTo(Server another){
         return (this.toString().compareTo(another.toString()));
     }
+
+    /**
+     * Returns a string representation of the server.
+     *
+     * @return A string indicating the server ID
+     */
     public String toString(){
-        return ("S"+id);
+        return ("S" + id);
     }
 }
